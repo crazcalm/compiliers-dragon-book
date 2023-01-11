f34427b57337c84703f4f4db6173d5f017337742
Marcus Willock <crazcalm@gmail.com>
2023-01-11 17:45:40 -0500

    Add Feed get_char and unget_char

--- chapter_2/code_examples/sample_compilier/src/main.rs
+++ chapter_2/code_examples/sample_compilier/src/main.rs
@@ -1,70 +1,17 @@
 use std::env;
 use std::fs::File;
-use std::io::{self, BufRead, BufReader, Read};
+use std::io::Read;
 use std::process::ExitCode;
-use std::str::Lines;
+
+mod feed;
+
+use crate::feed::Feed;
 
 fn get_input_string(path: &str) -> String {
     let mut result = String::new();
     let mut file = File::open(path).unwrap();
     file.read_to_string(&mut result).unwrap();
     result
-}
-
-struct InputFeed {
-    cursor: i32,
-    lineno: i32,
-    index: usize,
-    input: String,
-}
-
-impl InputFeed {
-    fn new(input: String) -> Self {
-        InputFeed {
-            cursor: 0,
-            lineno: 1,
-            index: 0,
-            input: input,
-        }
-    }
-
-    fn get_char(&mut self) -> Option<char> {
-        self.index += 1;
-
-        let mut characters = self.input.chars();
-
-        match characters.nth(self.index) {
-            Some(input_char) => {
-                if input_char.eq(&'\n') {
-                    self.lineno += 1;
-                    self.cursor = -1;
-                    return Some(input_char);
-                }
-
-                self.cursor += 1;
-                Some(input_char)
-            }
-            None => None,
-        }
-    }
-
-    fn unget_char(&mut self) -> Option<char> {
-        self.index -= 1;
-
-        let mut characters = self.input.chars();
-        match characters.nth(self.index) {
-            Some(input_char) => {
-                if input_char.eq(&'\n') {
-                    self.lineno -= 1;
-                }
-                // Ignoring cursor backtrack edge case...
-                self.cursor -= 1;
-
-                Some(input_char)
-            }
-            None => None,
-        }
-    }
 }
 
 fn main() -> ExitCode {
@@ -79,8 +26,8 @@ fn main() -> ExitCode {
         return ExitCode::FAILURE;
     }
 
-    let mut input_feed = InputFeed::new(input_string);
-    for _ in 0..100 {
+    let mut input_feed = feed::InputFeed::new(input_string);
+    for _ in 0..10 {
         print!("{:?}", input_feed.get_char());
     }
     println!();
