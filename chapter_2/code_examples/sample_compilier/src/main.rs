955040f1d6b12f0aaa8884fcecdd5c0459c2f4b5
Marcus Willock <crazcalm@gmail.com>
2023-01-11 18:19:44 -0500

    Start on Symbol table

--- chapter_2/code_examples/sample_compilier/src/main.rs
+++ chapter_2/code_examples/sample_compilier/src/main.rs
@@ -14,6 +14,45 @@ fn get_input_string(path: &str) -> String {
     result
 }
 
+#[derive(Debug, Clone)]
+pub enum TokenType {
+    NUM,
+    ID,
+}
+
+type Token = (TokenType, String);
+
+trait SymbolTableTrait {
+    fn lookup(self, value: String) -> Option<Token>;
+    fn insert(&mut self, token_type: TokenType, value: String);
+}
+
+struct SymbolTable {
+    inner: Vec<Token>,
+}
+
+impl SymbolTableTrait for SymbolTable {
+    fn lookup(self, value: String) -> Option<Token> {
+        for (token_type, token_value) in self.inner.iter() {
+            if value.eq(token_value) {
+                return Some((token_type.clone(), value));
+            }
+        }
+
+        None
+    }
+
+    fn insert(&mut self, token_type: TokenType, value: String) {
+        self.inner.push((token_type, value))
+    }
+}
+
+impl SymbolTable {
+    pub fn new() -> Self {
+        SymbolTable { inner: Vec::new() }
+    }
+}
+
 fn main() -> ExitCode {
     let mut args = env::args();
     let input_string: String;
